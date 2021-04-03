9687253f2405d12ac1c2c12d9540cf7fbbcf517f
Justy777 <justy777@gmail.com>
2021-04-03 02:20:03 -0300

    Restructure each year into a submodule

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
 #[macro_use]
 extern crate lazy_static;
 
-mod year_2015_day_1;
-mod year_2015_day_2;
-mod year_2015_day_3;
-mod year_2015_day_4;
-mod year_2015_day_5;
-mod year_2015_day_6;
-mod year_2015_day_7;
-mod year_2015_day_8;
+mod year_2015;
 
 #[cfg(test)]
 mod tests {}
